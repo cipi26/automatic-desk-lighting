4502798cffd790f7979b00ef4b3d9cbfec52305f
Ciprian Stefan <stefanciprian444@gmail.com>
2026-02-07 22:49:10 +0200

    Refactor DHT and LED pin definitions to use inline constexpr for better type safety

--- include/dht.h
+++ include/dht.h
@@ -1,5 +1,7 @@
 #pragma once
 
-#define DHT_PIN 18
+#include <Arduino.h>
+
+inline constexpr uint8_t DHT_PIN = 18;
 
 // DHT climateSensor(DHT_PIN, DHT11);
--- include/led_strip.h
+++ include/led_strip.h
@@ -2,8 +2,8 @@
 
 #include <FastLED.h>
 
-#define LED_PIN 5
-#define NUM_LEDS 60
+inline constexpr u_int8_t LED_PIN = 5;
+inline constexpr u_int16_t NUM_LEDS = 60;
 
 extern CRGB leds[];
 
